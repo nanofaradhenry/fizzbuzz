40cbc2f5fd3a7860344d7da4bd2bf22a9878fd72
nanofaradhenry <90361088+nanofaradhenry@users.noreply.github.com>
2021-09-08 18:07:57 -0700

    Add files via upload

---
+++ fizzybuzzy.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+for numbers in range(1,100):
+    if numbers%3==0 and numbers%5==0:
+        print("FizzBuzz")
+    elif numbers%3==0:
+        print("Fizz")
+    elif numbers%5==0:
+        print("Buzz")
+    else: print("numbers")
+   
